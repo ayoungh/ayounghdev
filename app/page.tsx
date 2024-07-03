751967ce9cc7bc781ddce1f5fc1533e2f0ac3674
Anthony Young <404328+ayoungh@users.noreply.github.com>
2024-07-03 18:24:51 +0100

    start nav

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,13 @@
+import Nav from "@/components/Nav";
+import { ThemeToggle } from "@/components/ThemeToggle";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      {/* <div className="absolute top-0 z-[-2] h-screen w-screen bg-neutral-950 bg-[radial-gradient(ellipse_80%_80%_at_50%_-20%,rgba(120,119,198,0.3),rgba(255,255,255,0))]"></div> */}
+      <Nav />
+
       <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
         <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
           Get started by editing&nbsp;
@@ -25,6 +30,8 @@ export default function Home() {
               priority
             />
           </a>
+
+          <ThemeToggle />
         </div>
       </div>
 
---
+++ components/Nav/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Nav() {
+  return (
+    <div className="max-w-6xl border w-full flex justify-center">
+        Ayoungh.dev
+    </div>
+  )
+}
